2889f1263121cffd7494091df6e6617b6a8865c7
Tyler Clemens <tyler.r.clemens@gmail.com>
2016-06-08 19:46:24 -0700

    remove handler stuff

--- lib/tielurs_heart_rate.ex
+++ lib/tielurs_heart_rate.ex
@@ -1,20 +1,10 @@
 defmodule TielursHeartRate do
   alias Alice.Conn
-  use Timex
 
   def measure(numberOfHeartRates \\ 1) do
     http.get!("").body
     |> Enum.reverse
     |> Enum.take(numberOfHeartRates)
-  end
-
-  def format_message(measurement) do
-    split_time = String.split(measurement.time, ":")
-    hours = String.to_integer(Enum.at(split_time, 0))
-    minutes = String.to_integer(Enum.at(split_time, 1))
-    seconds = String.to_integer(Enum.at(split_time, 2))
-    heart_rate_time = DateTime.set(DateTime.local, hour: hours, minute: minutes, second: seconds)
-    "Tielur's heart rate was #{measurement.value}BPM about #{Timex.from_now(heart_rate_time)}"
   end
 
   defp http do
