579f3bdc5e7f1bd22dcb6b59604fcfb596da5484
Tyler Clemens <tyler.r.clemens@gmail.com>
2016-06-08 19:48:32 -0700

    remove duplicate/bad tests

--- test/alice/tielurs_heart_rate_test.exs
+++ test/alice/tielurs_heart_rate_test.exs
@@ -1,14 +1,4 @@
 defmodule TielursHeartRateTest do
   use ExUnit.Case, async: true
 
-  test "process_url callback adds the domain to the path" do
-    assert TielursHeartRate.measure == [%{time: "18:23:00", value: 91}]
-  end
-
-  test "stuff" do
-    measurement = TielursHeartRate.measure
-    |> "Tielur's last recorded heart rate was: #{measurement.value}"
-    |> String.strip
-  end
-
 end
